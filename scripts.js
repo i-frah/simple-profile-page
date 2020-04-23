17c6fa1fd0421a6fd49529d78a5ae2fd7f007b91
ifrahv <ifrah.vermeer@gmail.com>
2020-04-23 18:19:34 +0100

    added starter code

---
+++ scripts.js
@@ -0,0 +1,18 @@
+
+/* SCRIPTS.JS */
+
+function changePic() {
+    document.getElementById("profilePic").src = "images/adrian.webp";
+  }
+  
+  function normalPic() {
+    document.getElementById("profilePic").src = "images/adrian.jpg";
+  }
+
+
+$(".go-to-contact").click(function() {
+    $('html, body').animate({
+        scrollTop: $(".contact").offset().top
+    }, 1000);
+});
+
